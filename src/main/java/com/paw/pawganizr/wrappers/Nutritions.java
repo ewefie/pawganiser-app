ae0087cb039baeba1613bcf19ccfc7a3af572272
Ewelina Fiedorowicz <ewelinaformella@gmail.com>
2020-02-07 20:05:49 +0100

    data annotation added

--- src/main/java/com/paw/pawganizr/wrappers/Nutritions.java
+++ src/main/java/com/paw/pawganizr/wrappers/Nutritions.java
@@ -2,10 +2,12 @@ package com.paw.pawganizr.wrappers;
 
 import com.paw.pawganizr.models.Nutrition;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.util.List;
 
 @AllArgsConstructor
+@Data
 public class Nutritions {
     private List<Nutrition> nutritions;
 }
